346a10fc711e342d2368c13d502edf8d5fcff551
Burak Karahan <diegowildfire@gmail.com>
2017-06-26 20:43:02 +0300

    Add fon-awesome icons

--- application/views/kod_view.php
+++ application/views/kod_view.php
@@ -5,7 +5,7 @@
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+            <h2 class="center light-blue-text"><i class="fa fa-try" aria-hidden="true"></i></h2>
             <h5 class="center">Hepsi Ücretsiz</h5>
 
             <p class="light">Eğitimler için başlangıçta yada bir süre sonra sizden ücret talep etmiyoruz. Para kazanmak için reklam dahi yayınlamıyoruz. Tamamen kamu hizmeti amaçlı bir şekilde öğrendiklerimizi size de öğretmek ana felsefemiz.</p>
@@ -14,7 +14,7 @@
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+            <h2 class="center light-blue-text"><i class="fa fa-users" aria-hidden="true"></i></h2>
             <h5 class="center">Kullanıcı Dostu</h5>
 
             <p class="light">Eğitim arayüzümüz kullanıcılardan geri bildirimlerle oluşturulmuş basit ve kullanışlılık fikrini benimsemektedir. Konu anlatımı, göreviniz, kod yazabileceğiniz düzenleyiciniz ve kodların önizlemesi aynı sayfada. Ayrıca kod düzenleyiciniz size yardımcı olmakta.</p>
@@ -23,7 +23,7 @@
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+            <h2 class="center light-blue-text"><i class="fa fa-crosshairs" aria-hidden="true"></i></h2>
             <h5 class="center">Tecrübeleyin</h5> 
 
             <p class="light">Öğrendiklerinizi hemen ardından uygulayarak daha kalıcı hale getirin. Öğrenin, uygulayın ve sonraki derse geçin. Herhangi bir dersten başlayabilir , dersleri atlayabilirsiniz. Bir derste takılırsanız kısa bir süre ara verip tekrar denemenizi tavsiye ederiz.</p>
